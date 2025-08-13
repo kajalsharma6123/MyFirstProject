da7a165b6090ff947915262e5d1b4f2b88e54f42
Kajal <kajalsharma6123@gmail.com>
2025-08-13 16:54:40 +0530

    Added second.js and updated index file

--- first.js
+++ first.js
@@ -1,1 +1,2 @@
-console.log('Helo');
+console.log('Hello');
+console.log("I am editing the spelling")
---
+++ second.js
@@ -0,0 +1,2 @@
+console.log("Hello my name is Kajal Sharma");
+console.log("I am learning GitHub");
